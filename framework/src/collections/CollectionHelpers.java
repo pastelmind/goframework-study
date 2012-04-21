4a3a234119f07e09af76f81adc8af9afa16d68c9
graveth <graveth@gmail.com>
2012-04-21 23:15:10 +0100

    Added new collections methods, removed 1.7

--- framework/src/collections/CollectionHelpers.java
+++ framework/src/collections/CollectionHelpers.java
@@ -1,13 +1,27 @@
 package collections;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Vector;
 
 public class CollectionHelpers {
 
-    @SuppressWarnings("unchecked")
-	public static <T> List<T> listOf( T... items ) {
+    public static <T> List<T> listOf( T... items ) {
         return Arrays.asList( items );
+    }
+
+    public static <T, U> HashMap<T, U> newHashMap() {
+        return new HashMap<T, U>();
+    }
+
+    public static <T> ArrayList<T> newArrayList() {
+        return new ArrayList<T>();
+    }
+
+    public static <T> Vector<T> newVector() {
+        return new Vector<T>();
     }
 
 }
